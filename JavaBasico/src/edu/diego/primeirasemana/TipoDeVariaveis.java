5322668c0cfb0a17bfc7a8e49558695c940b7721
Diego <diego.1996pessoal@gmail.com>
2025-04-18 01:31:38 -0300

    comentarios no código

--- JavaBasico/src/edu/diego/primeirasemana/TipoDeVariaveis.java
+++ JavaBasico/src/edu/diego/primeirasemana/TipoDeVariaveis.java
@@ -1,24 +1,25 @@
 package edu.diego.primeirasemana;
-public class TipoDeVariaveis{
-   
-    public static void main(String[] args){
-       //tipos primitivos  
-       byte idade = 123;
-       short ano = 2021;
-       int cep =21070333;
-       long cpf = 11154487596L;
-       float pi = 3.14F;
-       double salario = 1923.00;
 
-       // variaveis
-       int numero = 5;
-       numero =10;
-       System.out.print("Numero..:"+numero);
+public class TipoDeVariaveis {
 
-       // constantes
-       final double valor_De_pi = 3.14;
-      // valor_De_pi = 10; 
-      // o valor acima foi comentado pois não pode mudar valor de constante.
+  public static void main(String[] args) {
+    // tipos primitivos
+    byte idade = 123;
+    short ano = 2021;
+    int cep = 21070333;
+    long cpf = 11154487596L;
+    double salario = 300.2;
+    final float pi = 3.14F;
+    System.out.println("idade: " + idade + "ano: " + ano + "cep: " + cep + "cpf: " + cpf + "salario: " + salario);
+    System.out.println("O valor de pi é: " + pi);
+    // variaveis
+    int numero = 5;
+    numero = 10;
+    System.out.print("Numero..:" + numero);
 
-    }
+    // constantes
+    // pi = 10;
+    // o valor acima foi comentado pois não pode mudar valor de constante.
+
+  }
 }
